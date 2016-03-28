d1306c99c13518217f9b91f18fe24c748ff871e9
DDMing <ddming007@gmail.com>
2016-03-28 10:22:10 +0800

    添加MD5加密 并将数据库的密码改为密文。

--- src/main/java/TeamSeven/util/VerificationTool.java
+++ src/main/java/TeamSeven/util/VerificationTool.java
@@ -2,6 +2,8 @@ package TeamSeven.util;
 
 import TeamSeven.entity.Account;
 
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
 import java.sql.*;
 
 /**
@@ -18,6 +20,21 @@ public abstract class VerificationTool {
         String name = account.getUserName();
         String passwd = account.getPassword();
         return true;
+    }
+
+    public static String crypMD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+            }
+            System.out.println(sb.toString());
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return null;
     }
 
     public static int verifyPassword( String userName, String password ) //返回用户id,验证不通过返回-1
--- test/VerificationToolTest.java
+++ test/VerificationToolTest.java
@@ -3,6 +3,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.security.MessageDigest;
+
 import static org.junit.Assert.*;
 
 /**
@@ -23,7 +25,13 @@ public class VerificationToolTest {
     public void TestLogin() throws Exception{
         String userName = "a";
         String passwd = "bbb";
-        int ret = VerificationTool.verifyPassword(userName,passwd);
-        assertEquals(1,ret);
+        int ret = VerificationTool.verifyPassword(userName,VerificationTool.crypMD5(passwd) );
+        assertEquals(-1,ret);
+    }
+    @Test
+    public void TestCryp() throws Exception{
+        String origin = "haha";
+        String crypt = "4e4d6c332b6fe62a63afe56171fd3725";
+        assertEquals(crypt, VerificationTool.crypMD5(origin));
     }
 }
