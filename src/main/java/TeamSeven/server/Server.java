e3bc11a9782ee9edcb5c0e803b70d64625f5a47e
Josh Ouyang <JoshOY789@gmail.com>
2016-03-29 08:42:51 +0800

    将Server、Client端的命令行版本转移至新的类。

--- src/main/java/TeamSeven/server/Server.java
+++ src/main/java/TeamSeven/server/Server.java
@@ -3,15 +3,9 @@ package TeamSeven.server;
 import TeamSeven.server.socket.ServerSocket;
 import TeamSeven.server.socket.ServerSocketImpl;
 import org.java_websocket.WebSocketImpl;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.UnknownHostException;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-import javax.swing.*;
+
 import java.io.IOException;
+import java.net.UnknownHostException;
 
 /**
  * Created by joshoy on 16/3/22.
@@ -25,7 +19,6 @@ public class Server {
     // s.start();
 
     public Server() {
-
         // TODO: 端口可设置
         this.port = 8887;
 
@@ -35,8 +28,6 @@ public class Server {
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
-
-
     }
 
 
@@ -45,34 +36,5 @@ public class Server {
         /* 启动Server后 */
         new Server();
     }
-
-    /*
-    public static void mainOld(String[] args) throws IOException, InterruptedException {
-        WebSocketImpl.DEBUG = true;
-        int port = 8887;
-
-        System.out.println("[*] 服务器已启动, 端口" + port + ", 输入restart重启, 输入exit退出. 其他广播消息可直接输入.");
-        BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
-        ServerSocket s = new ServerSocketImpl(port);
-
-        while (true) {
-            String in = sysin.readLine();
-            if (in.equals("exit")) {
-                s.stop();
-                System.out.println("[*] 服务器已退出.");
-                break;
-            } else if (in.equals("restart")) {
-                s.stop();
-                s = new ServerSocketImpl(port);
-                s.start();
-                System.out.println("[*] 服务器已重启, 输入restart重启, 输入exit退出. 其他广播消息可直接输入.");
-            }
-            else {
-                System.out.println("[*] [系统广播] " + in);
-                s.sendToAll("[系统广播] " + in);
-            }
-        }
-    }
-    */
 
 }
